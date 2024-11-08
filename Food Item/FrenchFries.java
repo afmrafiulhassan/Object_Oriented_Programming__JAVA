49721d751ac69d8f1024b53d0acaabbe9431f056
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-08 19:25:47 +0600

    Add files via upload

---
+++ Food Item/FrenchFries.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class FrenchFries extends Foods{
+	public FrenchFries(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
