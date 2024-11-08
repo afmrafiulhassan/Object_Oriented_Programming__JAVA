c53e503786b729590da3d288c587b609d7a7453e
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-08 19:24:10 +0600

    Add files via upload

---
+++ Food Item/ChickenWings.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class ChickenWings extends Foods{
+	public ChickenWings(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
