0ea06a71b4205c7fdead7e49105ebd36b5501267
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-08 19:23:12 +0600

    Add files via upload

---
+++ Food Item/Burger.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class Burger extends Foods{
+	public Burger(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
