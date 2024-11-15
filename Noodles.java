f0d5ac98e631a2b4c8856365d7f3aa402c558737
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-16 00:06:40 +0600

    Add files via upload

---
+++ Noodles.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class Noodles extends Foods{
+	public Noodles(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
