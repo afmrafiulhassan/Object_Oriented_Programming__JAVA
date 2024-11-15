e57335d497104f352aa6a969c5053c8ccb1af20a
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-16 00:06:29 +0600

    Add files via upload

---
+++ Food Item/Momo.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class Momo extends Foods{
+	public Momo(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
