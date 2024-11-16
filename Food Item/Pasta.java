70125397b28e02cc15cbf1deb9f14f07f24d91d3
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-16 17:11:40 +0600

    Add files via upload

---
+++ Food Item/Pasta.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class Pasta extends Foods{
+	public Pasta(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
