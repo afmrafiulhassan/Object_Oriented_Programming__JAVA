aa12f0de5ec159d53b2f337396d725d7d3203e87
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-17 22:30:15 +0600

    Add files via upload

---
+++ Food Item/Soup.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class Soup extends Foods{
+	public Soup(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
