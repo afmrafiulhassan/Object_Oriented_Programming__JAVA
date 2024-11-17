13ed12d5552bc75729d3cb406c9aa29c659e6c22
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-17 22:29:55 +0600

    Add files via upload

---
+++ Food Item/Sandwich.java
@@ -0,0 +1,9 @@
+//package classes;
+//import java.lang.*;
+public class Sandwich extends Foods{
+	public Sandwich(String name, double price, int quantity, int numberOfBuy, double tax, double discount){
+		super(name, price, quantity, numberOfBuy, tax, discount);
+	}
+	public void showAll(){
+		super.showAll();
+}
